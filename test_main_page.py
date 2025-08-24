26ef73f096f787ac0879b06256cf4946b55885ae
Stenin Maxim <msteninn1401@gmail.com>
2025-08-24 19:05:35 +0300

    test main page with PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -6,13 +6,16 @@ import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+
 
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser) 
+   link = "http://selenium1py.pythonanywhere.com/"
+   page = MainPage(browser, link)
+   page.open()
+   page.go_to_login_page() 
