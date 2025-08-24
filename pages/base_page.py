2fd9fd7ab78444fa6685655ba9452f55a51a9280
Stenin Maxim <msteninn1401@gmail.com>
2025-08-24 18:54:47 +0300

    add base and main

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,12 +7,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser) 
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+    
+    def open(self):
+        self.browser.get(self.url)
